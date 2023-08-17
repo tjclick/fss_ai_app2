8c95e88281b38ef55d76df12ce147135e0091b01
윤승용 <jesus4me153@gmail.com>
2023-08-17 21:34:54 +0900

    Update home_chart.dart 8/17.21
    Update home_chart.dart 8/17.21

--- lib/modules/charts/home_chart.dart
+++ lib/modules/charts/home_chart.dart
@@ -4,41 +4,41 @@ import 'package:flutter/material.dart';
 import '../../models/home_model.dart';
 import '../../providers/home_provider.dart';
 
-class HomeItemChart extends StatefulWidget  {
-  final String ticker;                    // 상위 위젯 호출시 변수값 전달 받기 위함
-  HomeItemChart({required this.ticker});  // 상위 위젯 호출시 변수값 전달 받기 위함
+class HomeItemChart extends StatefulWidget {
+  final String ticker; // 상위 위젯 호출시 변수값 전달 받기 위함
+  HomeItemChart({required this.ticker}); // 상위 위젯 호출시 변수값 전달 받기 위함
 
   @override
   _HomeItemChartState createState() => _HomeItemChartState();
 }
 
 class _HomeItemChartState extends State<HomeItemChart> {
-  late String ticker;                     // 상위 위젯으로부터 호출시 변수값 전달 받기 위함
+  late String ticker; // 상위 위젯으로부터 호출시 변수값 전달 받기 위함
+  int _idxDivid = 350;
 
   HomeProviders homeProvider = HomeProviders();
   List<PredictedTickerData> pTickerData = [];
   List<PredictedTickerData> _data = [];
 
   Future<void> loadJsonData() async {
-    // provider에게 변수값으로 호출 연동 
+    // provider에게 변수값으로 호출 연동
     pTickerData = await homeProvider.getPredictedTickerData(ticker);
 
     setState(() {
       _data = pTickerData;
+      _idxDivid = (pTickerData.length / 3).toInt();
     });
   }
 
   @override
   void initState() {
     super.initState();
-    ticker = widget.ticker;  // 상위 위젯으로부터 호출시 변수값 전달 받기 위함
+    ticker = widget.ticker; // 상위 위젯으로부터 호출시 변수값 전달 받기 위함
 
     loadJsonData();
   }
 
-  
-
-@override
+  @override
   Widget build(BuildContext context) {
     final blue = charts.MaterialPalette.blue.shadeDefault;
     final red = charts.MaterialPalette.red.shadeDefault;
@@ -49,96 +49,111 @@ class _HomeItemChartState extends State<HomeItemChart> {
       new charts.Series(
         id: "ticker",
         data: _data,
-        colorFn: (PredictedTickerData series, _) => (series.xaxis/300) < 1 ? blue : (series.xaxis/300) < 2 ? yellow : red,
-        domainFn: (PredictedTickerData series, _) => series.xaxis,    // X축
-        measureFn: (PredictedTickerData series, _) => series.yprice,  // Y축
+        colorFn: (PredictedTickerData series, _) =>
+            (series.xaxis / _idxDivid) < 1
+                ? blue
+                : (series.xaxis / _idxDivid) < 2
+                    ? yellow
+                    : red,
+        domainFn: (PredictedTickerData series, _) => series.xaxis, // X축
+        measureFn: (PredictedTickerData series, _) => series.yprice, // Y축
+        strokeWidthPxFn: (_, __) => 1,
         //colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-      ), 
+      ),
     ];
 
     return Container(
-      height: 170,
-      //padding: EdgeInsets.all(10),
+      height: 180,
+      padding: EdgeInsets.only(top: 10.0),
       child: Card(
         color: Color.fromARGB(255, 0, 0, 0),
         // child: Padding(
-        //   padding: const EdgeInsets.all(5.0),
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: charts.LineChart(
-                  series, 
-                  animate: false,
-                  // X축 가이드라인, MAX/MIN 값 지정
-                  // domainAxis: new charts.OrdinalAxisSpec(
-                  //     // Make sure that we draw the domain axis line.
-                  //     showAxisLine: true,
-                  //     // But don't draw anything else.
-                  //     renderSpec: new charts.NoneRenderSpec(),
-                      
-                  // ),
-                  // Y축 가이드라인, MAX/MIN 값 지정
-                  primaryMeasureAxis: new charts.NumericAxisSpec(
-                      renderSpec: charts.GridlineRendererSpec(
-                                  lineStyle: charts.LineStyleSpec(dashPattern: [3, 3],),
-                                  //labelStyle: charts.TextStyleSpec(fontSize: 14, color: charts.MaterialPalette.white),
-                      ),
-                      tickProviderSpec:new charts.BasicNumericTickProviderSpec(zeroBound: false),
+        // padding: const EdgeInsets.only(top: 10.0),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: charts.LineChart(
+                series,
+                animate: false,
+                // X축 가이드라인, MAX/MIN 값 지정
+                // domainAxis: new charts.OrdinalAxisSpec(
+                //     // Make sure that we draw the domain axis line.
+                //     showAxisLine: true,
+                //     // But don't draw anything else.
+                //     renderSpec: new charts.NoneRenderSpec(),
+
+                // ),
+                // Y축 가이드라인, MAX/MIN 값 지정
+                primaryMeasureAxis: new charts.NumericAxisSpec(
+                  renderSpec: charts.GridlineRendererSpec(
+                    lineStyle: charts.LineStyleSpec(
+                      dashPattern: [3, 3],
+                    ),
+                    //labelStyle: charts.TextStyleSpec(fontSize: 14, color: charts.MaterialPalette.white),
                   ),
-
-                  
-                  behaviors: [
-                      // 블럭 영역 색상표시 지정 하기
-                      new charts.RangeAnnotation([
-                          new charts.RangeAnnotationSegment(
-                            0, 300, charts.RangeAnnotationAxisType.domain,
-                            startLabel: '어제',
-                            //endLabel: '오늘+D1',
-                            labelAnchor: charts.AnnotationLabelAnchor.end,
-                            labelStyleSpec: charts.TextStyleSpec(fontSize: 14, color: charts.MaterialPalette.white.darker),
-                            color: charts.MaterialPalette.gray.shade800,
-                            labelDirection: charts.AnnotationLabelDirection.horizontal),
-                          
-                          new charts.RangeAnnotationSegment(
-                            301, 600, charts.RangeAnnotationAxisType.domain,
-                            startLabel: '오늘/D+1',
-                            //endLabel: '오늘+D1',
-                            labelAnchor: charts.AnnotationLabelAnchor.end,
-                            labelStyleSpec: charts.TextStyleSpec(fontSize: 14, color: charts.MaterialPalette.white.darker),
-                            color: charts.MaterialPalette.black,
-                            labelDirection: charts.AnnotationLabelDirection.horizontal), 
-                          
-                          new charts.RangeAnnotationSegment(
-                            601, 900, charts.RangeAnnotationAxisType.domain,
-                            startLabel: 'D+2',
-                            //endLabel: '오늘+D1',
-                            labelAnchor: charts.AnnotationLabelAnchor.end,
-                            labelStyleSpec: charts.TextStyleSpec(fontSize: 14, color: charts.MaterialPalette.white.darker),
-                            color: charts.MaterialPalette.black,
-                            labelDirection: charts.AnnotationLabelDirection.horizontal),
-
-                      //   new charts.LineAnnotationSegment(
-                      //       70000, charts.RangeAnnotationAxisType.measure,
-                      //       //startLabel: 'Measure 2 Start',
-                      //       endLabel: 'High',
-                      //       color: charts.MaterialPalette.gray.shade500,),
-                        ], defaultLabelPosition: charts.AnnotationLabelPosition.margin
-                      ),
-                      // 라인 선택 하이라이트
-                      // new charts.LinePointHighlighter(
-                      //     showHorizontalFollowLine:charts.LinePointHighlighterFollowLineType.none,
-                      //     showVerticalFollowLine:charts.LinePointHighlighterFollowLineType.nearest),
-                      // new charts.SelectNearest(eventTrigger: charts.SelectionTrigger.tapAndDrag),
-
-                    ],
-
+                  tickProviderSpec:
+                      new charts.BasicNumericTickProviderSpec(zeroBound: false),
                 ),
-              )
-            ],
-          ),
+
+                behaviors: [
+                  // 블럭 영역 색상표시 지정 하기
+                  new charts.RangeAnnotation([
+                    new charts.RangeAnnotationSegment(
+                        0, _idxDivid, charts.RangeAnnotationAxisType.domain,
+                        startLabel: '어제',
+                        //endLabel: '오늘+D1',
+                        labelAnchor: charts.AnnotationLabelAnchor.end,
+                        labelStyleSpec: charts.TextStyleSpec(
+                            fontSize: 14,
+                            color: charts.MaterialPalette.white.darker),
+                        color: charts.MaterialPalette.gray.shade800,
+                        labelDirection:
+                            charts.AnnotationLabelDirection.horizontal),
+
+                    new charts.RangeAnnotationSegment((_idxDivid),
+                        (_idxDivid * 2), charts.RangeAnnotationAxisType.domain,
+                        startLabel: '오늘/D+1',
+                        //endLabel: '오늘+D1',
+                        labelAnchor: charts.AnnotationLabelAnchor.end,
+                        labelStyleSpec: charts.TextStyleSpec(
+                            fontSize: 14,
+                            color: charts.MaterialPalette.white.darker),
+                        color: charts.MaterialPalette.black,
+                        labelDirection:
+                            charts.AnnotationLabelDirection.horizontal),
+
+                    new charts.RangeAnnotationSegment((_idxDivid * 2),
+                        (_idxDivid * 3), charts.RangeAnnotationAxisType.domain,
+                        startLabel: 'D+2',
+                        //endLabel: '오늘+D1',
+                        labelAnchor: charts.AnnotationLabelAnchor.end,
+                        labelStyleSpec: charts.TextStyleSpec(
+                            fontSize: 14,
+                            color: charts.MaterialPalette.white.darker),
+                        color: charts.MaterialPalette.black,
+                        labelDirection:
+                            charts.AnnotationLabelDirection.horizontal),
+
+                    //   new charts.LineAnnotationSegment(
+                    //       70000, charts.RangeAnnotationAxisType.measure,
+                    //       //startLabel: 'Measure 2 Start',
+                    //       endLabel: 'High',
+                    //       color: charts.MaterialPalette.gray.shade500,),
+                  ],
+                      defaultLabelPosition:
+                          charts.AnnotationLabelPosition.margin),
+                  // 라인 선택 하이라이트
+                  // new charts.LinePointHighlighter(
+                  //     showHorizontalFollowLine:charts.LinePointHighlighterFollowLineType.none,
+                  //     showVerticalFollowLine:charts.LinePointHighlighterFollowLineType.nearest),
+                  // new charts.SelectNearest(eventTrigger: charts.SelectionTrigger.tapAndDrag),
+                ],
+              ),
+            )
+          ],
+        ),
         // ),
       ),
     );
   }
-
 }
